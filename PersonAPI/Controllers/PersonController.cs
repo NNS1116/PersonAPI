ef3a1271d8f8cb661dd991843bba094fd9e82fb8
Nahlan <nahlan-nahla@outlook.com>
2024-09-20 17:43:52 -0400

    Add project files.

---
+++ PersonAPI/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonLibrary;
+
+namespace PersonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet("getPhoneNumber")]
+        public IActionResult GetPhoneNumber()
+        {
+            Student student = new Student();
+            string phoneNumber = student.GetCellPhoneNumber();
+            return Ok(phoneNumber); 
+        }
+    }
+}
